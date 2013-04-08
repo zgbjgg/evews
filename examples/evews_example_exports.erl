44c3dfe531822c30a87743211d13b7ac74f171cc
Jorge Garrido <jorge.garrido@morelosoft.com>
2013-04-08 15:04:49 -0500

    prints parametrized module exports

---
+++ examples/evews_example_exports.erl
@@ -0,0 +1,71 @@
+% ==========================================================================================================
+% 
+% EvE ws
+%
+% Copyright (C) 2013, Jorge Garrido <zgbjgg@gmail.com>. 
+% All rights reserved.
+%
+% BSD License
+%
+% Redistribution and use in source and binary forms, with or without modification, are permitted provided
+% that the following conditions are met:
+%
+%  * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+%        following disclaimer.
+%  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+%        the following disclaimer in the documentation and/or other materials provided with the distribution.
+%  * Neither the name of the authors nor the names of its contributors may be used to endorse or promote
+%        products derived from this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+% WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+% ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+% ==========================================================================================================
+-module(evews_example_exports).
+
+-export([start/1, loop/1]).
+
+%% starts the evews websocket server on the given port
+start(Port) ->
+    %% We need port, but also the callback module (in this case 'evews_module') and the callback function
+    %% on the module (in this case 'loop').
+    evews_sup:start_link([{port, Port}, {ws_handler, [{callback_m, ?MODULE}, {callback_f, loop}]}]).
+
+%% receive and sends messages with the process spawned on start_link
+%% Ws contains all info about socket and let send/receive messages.
+%% This process terminates when disconnect from browser is detected!.
+loop(Ws) ->
+    receive
+	%% this messages is received from the browser, Data is a binary that can be decoded with 
+	%% Ws:get(Data) to get only the message.
+	{browser, Data} ->
+	    %% call this to print functions exported on Ws
+	    ws_export(Ws),
+	    io:format("receive ~p\n", [Ws:get(Data)]),
+	    loop(Ws);
+	Any ->
+	    io:format("any ~p\n", [Any]),
+            loop(Ws)
+	%% after one second sends a message to the browser, then use Ws:send(["pushing!"]) 
+        after 1000 ->
+	    Ws:send(["pushing!"]),
+	    loop(Ws)
+    end.
+
+%% prints all functions exports on the parametrized module
+ws_export(Ws) ->
+    %% prints socket
+    io:format("socket: ~p\n", [Ws:socket()]),
+    %% prints peername 
+    io:format("peername: ~p\n", [Ws:peername()]),
+    %% prints port
+    io:format("port: ~p\n", [Ws:port()]),
+    %% prints sockname
+    io:format("sockname: ~p\n", [Ws:sockname()]).
+
+    
